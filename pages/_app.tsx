52ec2b4217974c24f08ca94a6505fd00b527c4a9
Jorge Oehrens <jorge.oehrens@gmail.com>
2024-07-16 19:06:14 -0400

    Create components Footer and Header (header complete 90%)

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,13 @@
+
 import type { AppProps } from 'next/app'
- import '@/styles/globals.css'
+import '@/styles/globals.css'
+import Layout2 from '@/layouts/Layout'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <Layout2> 
+      <Component {...pageProps} />
+  </Layout2>
+)
+
 }
